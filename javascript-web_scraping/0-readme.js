267a0d43a540af9e13613427d9a064653cb41cd2
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-05-19 17:47:42 +0400

    readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
+
 const fs = require("fs");
 
-const filePath = process.argv[2];
-
-fs.readFile(filePath, "utf-8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log(data);
+fs.readFile(`${process.argv[2]}`, "utf-8", (err, inputD) => {
+  if (err) throw err;
+  console.log(inputD.toString());
 });
